8243689303f30cb9ea779a91bb93c148dc23c101
KJ <kelaja@web.de>
2020-11-03 18:53:46 +0100

    test client server by KS

--- catkin_ws/build/gtest/googlemock/gtest/cmake_install.cmake
+++ catkin_ws/build/gtest/googlemock/gtest/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/home/steffy/mobile_robots/catkin_ws/install")
+  set(CMAKE_INSTALL_PREFIX "/home/kj/catkin_ws/install")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
