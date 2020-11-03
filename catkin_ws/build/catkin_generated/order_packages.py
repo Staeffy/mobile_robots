47fb2d38a98075fd2e28634417a3fc8433aa171e
steffy <stefany.greiner@gmail.com>
2020-11-03 13:37:16 -0500

    delete build and devel folders

--- catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/steffy/mobile_robots/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/steffy/mobile_robots/catkin_ws/devel;/home/steffy/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/steffy/mobile_robots/catkin_ws/devel;/home/steffy/catkin_ws/devel;/opt/ros/melodic' != '' else []
